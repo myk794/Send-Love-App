81834e1df90e0f68bc864e13fd37c48c0e53d3d9
Yigit <mykayam794@gmail.com>
2025-05-25 22:08:29 +0300

    Update

--- components/config.jsx
+++ components/config.jsx
@@ -1,20 +1,22 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID
+} from '@env';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA4CCrIQdpn6DQc2wZ_n1F1xZeh-aQu_ko",
-  authDomain: "send-love-624f6.firebaseapp.com",
-  projectId: "send-love-624f6",
-  storageBucket: "send-love-624f6.firebasestorage.app",
-  messagingSenderId: "655749714262",
-  appId: "1:655749714262:web:5654c539db3c2efe87c6f7"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const db = getDatabase(app);
